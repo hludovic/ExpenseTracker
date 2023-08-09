1e64a02c0930e1a366018919d153238908cd317a
Ludovic HENRY <badsteam@hotmail.com>
2023-08-09 01:14:03 -0400

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Ludovic HENRY on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
