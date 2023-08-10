3daec535ec7303b9ba37534c5e108aae756c75ad
Ludovic HENRY <badsteam@hotmail.com>
2023-08-09 22:32:16 -0400

    Refactor the MenuItem in the content view to use Enum

--- ExpenseTracker/ContentView.swift
+++ ExpenseTracker/ContentView.swift
@@ -7,21 +7,67 @@
 
 import SwiftUI
 
+enum MenuItem: Identifiable, CaseIterable {
+    case dashboard, income, expense, settings
+
+    var id: UUID {
+        UUID()
+    }
+
+    var title: String {
+        switch self {
+        case .dashboard:
+            return "Dashboard"
+        case .income:
+            return "Income"
+        case .expense:
+            return "Expenses"
+        case .settings:
+            return "Settings"
+        }
+    }
+
+    var image: String {
+        switch self {
+        case .dashboard:
+            return "chart.xyaxis.line"
+        case .income:
+            return "chart.pie"
+        case .expense:
+            return "chart.line.downtrend.xyaxis"
+        case .settings:
+            return "gear"
+        }
+    }
+
+    var view: some View {
+        switch self {
+        case .dashboard:
+            return Text("Dashboard")
+        case .income:
+            return Text("Income")
+        case .expense:
+            return Text("Expenses")
+        case .settings:
+            return Text("Settings")
+        }
+
+    }
+}
+
 struct ContentView: View {
-    @State private var menuItems = ["Dashboard", "Incomes", "Expenses", "Settings"]
-    @State private var selection: String?
+    @State private var menuItems: [MenuItem] = MenuItem.allCases
+    @State private var selection: MenuItem?
     var body: some View {
         NavigationSplitView {
-            List(menuItems, id: \.self, selection: $selection) { item in
-                HStack {
-                    Image(systemName: "person")
-                    Text(item)
+            List(menuItems, selection: $selection) { item in
+                NavigationLink(value: item){
+                    Label(item.title, systemImage: item.image)
                 }
             }
-            .navigationTitle(Text("Title"))
         } detail: {
             if let selected = selection {
-                Text(selected)
+                selected.view
             } else {
                 Text("Select an item")
             }
